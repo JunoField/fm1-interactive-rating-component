4b7c7aa9bf8bad5dba441c113d22de67767d41dd
JunoField <junofield123@gmail.com>
2022-04-29 23:42:51 +0100

    started selected radio colours but fucked hover in doing so, need to fix.

--- thanks.js
+++ thanks.js
@@ -1,7 +1,38 @@
+lightGrey = "hsl(217, 12%, 63%)";
+mediumGrey = "hsl(216, 12%, 54%)";
+circleBgnd = "#282e36";
+
+function resetRadioColours(){
+    document.getElementById("circle-one").style.setProperty("background-color", circleBgnd);
+    document.getElementById("circle-two").style.setProperty("background-color", circleBgnd);
+    document.getElementById("circle-three").style.setProperty("background-color", circleBgnd);
+    document.getElementById("circle-four").style.setProperty("background-color", circleBgnd);
+    document.getElementById("circle-five").style.setProperty("background-color", circleBgnd);
+    document.getElementById("circle-one").style.setProperty("color", lightGrey);
+    document.getElementById("circle-two").style.setProperty("color", lightGrey);
+    document.getElementById("circle-three").style.setProperty("color", lightGrey);
+    document.getElementById("circle-four").style.setProperty("color", lightGrey);
+    document.getElementById("circle-five").style.setProperty("color", lightGrey);
+}
+
+function handleRad1Change(){
+    resetRadioColours();
+    document.getElementById("circle-one").style.setProperty("background-color", mediumGrey);
+    document.getElementById("circle-one").style.setProperty("color", "white");
+}
+
+function handleRad2Change(){
+    resetRadioColours();
+    document.getElementById("circle-two").style.setProperty("background-color", mediumGrey);
+    document.getElementById("circle-two").style.setProperty("color", "white");
+}
+
+
+
 function submitClicked(event){
     document.getElementById("centre-card-select").style.display = "none";
     document.getElementById("centre-card-thanks").style.display = "block";
-    let rating = 9;
+    let rating = unknown;
 
     if (document.getElementById("radio-one").checked){
         rating = 1;
