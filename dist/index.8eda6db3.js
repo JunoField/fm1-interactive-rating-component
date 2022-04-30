5f65554c2baa77049c6bfcd5614c86a82ad27ca8
JunoField <junofield123@gmail.com>
2022-04-30 18:24:01 +0100

    done, besides possibly mobile view

--- dist/index.8eda6db3.js
+++ dist/index.8eda6db3.js
@@ -1,32 +1,35 @@
-lightGrey = "hsl(217, 12%, 63%)";
-mediumGrey = "hsl(216, 12%, 54%)";
-circleBgnd = "#282e36";
-function resetRadioColours() {
-    document.getElementById("circle-one").style.setProperty("background-color", circleBgnd);
-    document.getElementById("circle-two").style.setProperty("background-color", circleBgnd);
-    document.getElementById("circle-three").style.setProperty("background-color", circleBgnd);
-    document.getElementById("circle-four").style.setProperty("background-color", circleBgnd);
-    document.getElementById("circle-five").style.setProperty("background-color", circleBgnd);
-    document.getElementById("circle-one").style.setProperty("color", lightGrey);
-    document.getElementById("circle-two").style.setProperty("color", lightGrey);
-    document.getElementById("circle-three").style.setProperty("color", lightGrey);
-    document.getElementById("circle-four").style.setProperty("color", lightGrey);
-    document.getElementById("circle-five").style.setProperty("color", lightGrey);
+function EnableButtonResetColours() {
+    document.getElementById("submit-button").disabled = false;
+    document.getElementById("circle-one").classList.remove("select-circle-clicked");
+    document.getElementById("circle-two").classList.remove("select-circle-clicked");
+    document.getElementById("circle-three").classList.remove("select-circle-clicked");
+    document.getElementById("circle-four").classList.remove("select-circle-clicked");
+    document.getElementById("circle-five").classList.remove("select-circle-clicked");
 }
 function handleRad1Change() {
-    resetRadioColours();
-    document.getElementById("circle-one").style.setProperty("background-color", mediumGrey);
-    document.getElementById("circle-one").style.setProperty("color", "white");
+    EnableButtonResetColours();
+    document.getElementById("circle-one").classList.add("select-circle-clicked");
 }
 function handleRad2Change() {
-    resetRadioColours();
-    document.getElementById("circle-two").style.setProperty("background-color", mediumGrey);
-    document.getElementById("circle-two").style.setProperty("color", "white");
+    EnableButtonResetColours();
+    document.getElementById("circle-two").classList.add("select-circle-clicked");
+}
+function handleRad3Change() {
+    EnableButtonResetColours();
+    document.getElementById("circle-three").classList.add("select-circle-clicked");
+}
+function handleRad4Change() {
+    EnableButtonResetColours();
+    document.getElementById("circle-four").classList.add("select-circle-clicked");
+}
+function handleRad5Change() {
+    EnableButtonResetColours();
+    document.getElementById("circle-five").classList.add("select-circle-clicked");
 }
 function submitClicked(event) {
     document.getElementById("centre-card-select").style.display = "none";
     document.getElementById("centre-card-thanks").style.display = "block";
-    let rating = unknown;
+    let rating = "unknown";
     if (document.getElementById("radio-one").checked) rating = 1;
     else if (document.getElementById("radio-two").checked) rating = 2;
     else if (document.getElementById("radio-three").checked) rating = 3;
