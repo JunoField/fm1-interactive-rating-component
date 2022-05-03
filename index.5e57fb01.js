b8ab77a32a17e8a4561c7d6a4851eac222d41b3f
JunoField <junofield123@gmail.com>
2022-05-03 17:35:11 +0000

    Deployment update from main commit d9129ee with message: added node_modules and parcel cache to gitignore

--- index.5e57fb01.js
+++ index.5e57fb01.js
@@ -1,3 +1,4 @@
+let rating = 0;
 function EnableButtonResetColours() {
     document.getElementById("submit-button").disabled = false;
     for(let i = 1; i <= 5; i++)document.getElementById("circle-" + i).classList.remove("select-circle-clicked");
@@ -5,17 +6,14 @@ function EnableButtonResetColours() {
 function handleRadChange(buttonNumber) {
     EnableButtonResetColours();
     document.getElementById("circle-" + buttonNumber).classList.add("select-circle-clicked");
+    rating = buttonNumber;
 }
 function submitClicked(event) {
-    document.getElementById("centre-card-select").style.display = "none";
-    document.getElementById("centre-card-thanks").style.display = "block";
-    let rating = "unknown";
-    if (document.getElementById("radio-one").checked) rating = 1;
-    else if (document.getElementById("radio-two").checked) rating = 2;
-    else if (document.getElementById("radio-three").checked) rating = 3;
-    else if (document.getElementById("radio-four").checked) rating = 4;
-    else if (document.getElementById("radio-five").checked) rating = 5;
-    document.getElementById("selection-feedback-box").innerHTML = 'You selected ' + rating + ' out of 5';
+    if (rating > 0) {
+        document.getElementById("centre-card-select").style.display = "none";
+        document.getElementById("centre-card-thanks").style.display = "block";
+        document.getElementById("selection-feedback-box").innerHTML = 'You selected ' + rating + ' out of 5';
+    }
 }
 
 //# sourceMappingURL=index.5e57fb01.js.map
