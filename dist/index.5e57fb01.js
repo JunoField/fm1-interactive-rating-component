662d1d687bc559f4ac950fc8d3b3d625cedfdce9
JunoField <junofield123@gmail.com>
2022-05-04 11:09:44 +0100

    fixed gitignore and node-modules folder

---
+++ dist/index.5e57fb01.js
@@ -0,0 +1,19 @@
+let rating = 0;
+function EnableButtonResetColours() {
+    document.getElementById("submit-button").disabled = false;
+    for(let i = 1; i <= 5; i++)document.getElementById("circle-" + i).classList.remove("select-circle-clicked");
+}
+function handleRadChange(buttonNumber) {
+    EnableButtonResetColours();
+    document.getElementById("circle-" + buttonNumber).classList.add("select-circle-clicked");
+    rating = buttonNumber;
+}
+function submitClicked(event) {
+    if (rating > 0) {
+        document.getElementById("centre-card-select").style.display = "none";
+        document.getElementById("centre-card-thanks").style.display = "block";
+        document.getElementById("selection-feedback-box").innerHTML = 'You selected ' + rating + ' out of 5';
+    }
+}
+
+//# sourceMappingURL=index.5e57fb01.js.map
