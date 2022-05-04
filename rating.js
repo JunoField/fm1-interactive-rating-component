c217b8afbfc8becf0efa6576a34ed278aee0dcda
JunoField <junofield123@gmail.com>
2022-05-04 11:51:52 +0100

    - removed EnableButtonResetColours - added contents to other function

--- rating.js
+++ rating.js
@@ -1,14 +1,11 @@
 let rating = 0;
 
-function EnableButtonResetColours(){
+function handleRadChange(buttonNumber){
     document.getElementById("submit-button").disabled = false;
     for (let i = 1; i <= 5; i++){
         document.getElementById("circle-" + i).classList.remove("select-circle-clicked");
     }
-}
 
-function handleRadChange(buttonNumber){
-    EnableButtonResetColours();    
     document.getElementById("circle-" + buttonNumber).classList.add("select-circle-clicked");
     rating = buttonNumber;
 }
